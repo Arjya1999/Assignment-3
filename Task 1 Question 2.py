26825dcc3db0a76ef5429ca0cf31712aa5e02617
Arjya1999 <42546158+Arjya1999@users.noreply.github.com>
2019-10-19 08:15:51 +0530

    Add files via upload

---
+++ Task 1 Question 2.py
@@ -0,0 +1,7 @@
+subjects=["Americans","Indians"]
+verbs=["play","watch"]
+objects=["Baseball","Cricket"]
+for i in subjects:
+    for j in verbs:
+        for k in objects:
+            print(i+" "+j+" "+k)
